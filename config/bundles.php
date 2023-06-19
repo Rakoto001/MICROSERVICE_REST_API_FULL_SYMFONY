6b23f13bbe94c281045793479dbed0fd64694544
Dan <danielorak1@gmail.com>
2023-06-19 20:52:06 +0000

    add securization to API JWT TOKEN

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
